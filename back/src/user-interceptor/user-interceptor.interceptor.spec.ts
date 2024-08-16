81db33bbe73f167bc2ecf1ac55715f2143277b1f
agusdor96 <miroetto12@gmail.com>
2024-08-16 17:25:02 -0300

    add user interceptor

---
+++ back/src/user-interceptor/user-interceptor.interceptor.spec.ts
@@ -0,0 +1,7 @@
+import { UserInterceptorInterceptor } from './user-interceptor.interceptor';
+
+describe('UserInterceptorInterceptor', () => {
+  it('should be defined', () => {
+    expect(new UserInterceptorInterceptor()).toBeDefined();
+  });
+});
